8b7bbe2307ca33af5971faf037af78d88d5412e6
Shariya31 <abushariya31@gmail.com>
2024-11-23 20:12:27 +0530

    integrated socket io for real time updates on task creation and task updates

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+import http from 'http';
+import dotenv from 'dotenv';
+import { Server } from 'socket.io';
 import express from 'express'
 import cors from 'cors'
-import dotenv from 'dotenv'
 import { connectDB } from './config/db.js';
 import { errorMiddleware } from './middlewares/error.js';
 
 //Routes import
 import authRoutes from './routes/auth.js'
 import userRoutes from './routes/user.js'
+import taskRoutes from './routes/task.js'
 
 dotenv.config();
 
@@ -17,6 +20,37 @@ connectDB(mongoUri)
 
 const app = express();
 
+// Create HTTP server from the Express app
+const server = http.createServer(app);
+
+// Set up Socket.IO
+const io = new Server(server, {
+    cors: {
+        origin: '*', // Allow all origins (adjust for production security)
+    },
+});
+
+
+io.on('connection', (socket) => {
+    console.log('A user connected:', socket.id);
+
+    // Real-time task creation
+    socket.on('taskCreated', (data) => {
+        console.log('Task Created:', data);
+        io.emit('createTask', data); // Broadcast to all connected clients
+    });
+
+    // Real-time task updates
+    socket.on('taskUpdated', (data) => {
+        console.log('Task updated:', data);
+        io.emit('updateTasks', data); // Broadcast to all connected clients
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected:', socket.id);
+    });
+});
+
 
 // middlewares
 app.use(cors());
@@ -26,13 +60,21 @@ app.get('/', (req, res)=>{
     res.send('running')
 })
 
-app.use('/api/v1/auth', authRoutes)
-app.use('/api/v1/user', userRoutes)
+app.use((req, res, next) => {
+    req.io = io;
+    next();
+});
+
+
+app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/user', userRoutes);
+app.use('/api/v1/task', taskRoutes);
 
 app.use(errorMiddleware)
-const PORT = process.env.PORT || 4000
-app.listen(PORT, ()=>{
-    console.log(`App is running on http://localhost:${PORT}`)
-})
 
-export default app
+// Start the server
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+    console.log(`App is running on http://localhost:${PORT}`);
+});
+
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -9,12 +9,12 @@ export const protect = (req, res, next)=>{
     }
 
     if(!token){
-        return next(new Errorhandler("Unauthorized User", 401))
+        return next(new Errorhandler("Unauthorized User | Please signup and login first", 401))
     }
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
-    if(!decoded) return next(new Errorhandler("Unauthorized User", 401))
+    if(!decoded) return next(new Errorhandler("Unauthorized User | Invalid Token", 401))
     
     req.user = decoded.id
 
