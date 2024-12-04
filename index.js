b92149d762e7592bdcca6be8734909f45fd101b5
Shariya31 <abushariya31@gmail.com>
2024-12-04 15:22:23 +0530

    initialized server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("hello")
+import express from 'express'
+const PORT = 2300;
+const app = express();
+
+app.get('/', (req, res)=>{
+    res.send("hello")
+})
+
+
+app.listen(PORT, ()=>{
+    console.log(`App is running on http://localhost:${PORT}`)
+})
