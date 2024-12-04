0afb782430cab9bb3035bd65b64d29eacb6b4d0e
Shariya31 <abushariya31@gmail.com>
2024-12-04 14:26:26 +0530

    initialized project

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-    testEnvironment: "node", // Ensure Node.js environment
-    testMatch: ["**/?(*.)+(spec|test).[tj]s?(x)"], // Match test files
-    testPathIgnorePatterns: ["/node_modules/"], // Ignore `node_modules`
-    transform: {
-        '^.+\\.js$': 'babel-jest',
-    },
-};
