88d9684bab4c754f6a4f27bf52e5b852b8466059
Shariya31 <abushariya31@gmail.com>
2024-12-15 14:28:39 +0530

    initialized test and ci environment

---
+++ models/_tests_/sample.test.js
@@ -0,0 +1,10 @@
+import request from 'supertest';
+import app from '../../app.js';
+
+describe('Sample Test', () => {
+    it('should return a success message', async () => {
+        const res = await request(app).get('/');
+        expect(res.statusCode).toBe(200);
+        expect(res.text).toBe('Server is working!');
+    });
+});
